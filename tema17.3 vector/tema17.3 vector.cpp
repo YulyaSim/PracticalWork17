993ec32268fc2a399e9b7eb36c3dfbebd8dad412
YulyaSim <152449597+YulyaSim@users.noreply.github.com>
2024-02-08 17:03:34 +1100

    tema17.3 vector
    tema17.3 vector

---
+++ tema17.3 vector/tema17.3 vector.cpp
@@ -0,0 +1,42 @@
+﻿// tema17.3 vector.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+class Vector
+{
+private:
+
+    double x;
+    double y;
+    double z;
+
+public:
+    Vector() : x(0), y(0), z(0)
+    { }
+    Vector(double _x, double _y, double _z) : x(_x), y(_y), z(_z)
+    {}
+  
+    double size()
+    {
+        return sqrt(x * x + y * y + z * z);
+    }
+
+    void Show()
+    {
+        cout << "\n" << x << " " << y << " " << z;
+    }
+   
+};
+
+int main()
+{
+    Vector v;
+    v.Show();
+    Vector V2(10, 20, 30);
+    V2.Show();
+    cout <<"\n" <<  V2.size();
+}
